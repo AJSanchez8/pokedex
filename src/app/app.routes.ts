7637fd937b174df516cfd99541fb4882ce330891
AjSanchez8 <antoniojosesanchezgonzalez8@gmail.com>
2024-03-28 13:16:29 +0100

    Página de tipos y de error

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,9 +2,13 @@ import { Component } from '@angular/core';
 import { Routes } from '@angular/router';
 import { PrincipalComponent } from './components/principal/principal.component';
 import { CuerpoComponent } from './components/cuerpo/cuerpo.component';
+import { TiposComponent } from './components/tipos/tipos.component';
+import { ErrorComponent } from './components/error/error.component';
 
 export const routes: Routes = [
     {path:"", component:PrincipalComponent},
-    {path:"pokemon",component:CuerpoComponent}
+    {path:"pokemon",component:CuerpoComponent},
+    {path:"tipos", component:TiposComponent },
+    {path:"**",component:ErrorComponent}
 
 ];
